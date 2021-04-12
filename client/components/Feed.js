a3cf8d2c9b4734e02cedac518bb14d402fac42a5
Adam Karlsten <adamkarlsten@gmail.com>
2021-04-12 16:53:07 +0200

    Update Feed.js

--- client/components/Feed.js
+++ client/components/Feed.js
@@ -44,7 +44,7 @@ const Feed = () => {
         {data?.map(event => (
           <FeedItem key={event._id} event={event} user={user} />
         ))}
-        {!loadingGroups && data?.length = 0 && (
+        {!loadingGroups && data?.length === 0 && (
           <p>No recent activity! Only the last 7 days are displayed.</p>
         )}
       </div>
